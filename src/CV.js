e279de6736ad54d0b290dbbd74fb8b772a1a0f72
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-04-26 19:10:32 +0300

    Base for website

---
+++ src/CV.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function CV() {
+return (
+    <div>
+        <h1>CV</h1>
+    </div>
+  );
+}
+
+export default CV;
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+function Nav() {
+    return (
+        <nav>
+            <h3>Logo</h3>
+            <ul className="nav-links">
+                <li>About Me</li>
+                <li>CV</li>
+                <li>Projects</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
