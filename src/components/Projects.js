d61afb9dcf790c341f8d1cc6cd08aefed28a43e1
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-05-02 19:05:48 +0300

    Base for fetching github repos

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 import './App.css';
 
+// TODO: Continue from here
+// * Get individual datas from data to be shown url example
+const data = fetch('https://api.github.com/users/SamiHei/repos')
+.then(response => response.json());
+
+console.log(data);
+
 function Projects() {
     return (
         <div className="page-3">
