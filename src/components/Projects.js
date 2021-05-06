603229612193bc2911026d35d92b06fffdeb4f97
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-05-06 19:04:33 +0300

    Github scroll list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,11 +20,26 @@ function Projects() {
         console.log(projects);
     },[])
 
+    const Arrow = ({ text, className }) => {
+        return (
+            <div
+                className={className}
+            >{text}</div>
+        );
+    };
+
+    // Testing left and right arrow
+    const ArrowLeft = Arrow({ text: '<', className: 'arrow-prev' });
+    const ArrowRight = Arrow({ text: '>', className: 'arrow-next' });
+
     return (
         <div className="page-3">
             <h1>Projects</h1>
             <div className="test">
-                <ScrollMenu data={projects.map(project => <div>{project.html_url}</div>)}/>
+                <ScrollMenu data={projects.map(project => <div key={project.name}>{project.name}</div>)}
+                arrowLeft={ArrowLeft}
+                arrowRight={ArrowRight}
+                />
             </div>
         </div>
     );
@@ -34,3 +49,5 @@ export default Projects;
 
 // example for scroll
 // https://www.npmjs.com/package/react-horizontal-scrolling-menu
+
+// TODO: Create list of the items for the menu that are in <div></div> and styled
