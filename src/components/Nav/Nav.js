cfed3354470d94be7210c9ab43b78dfb26ec82e1
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-05-11 19:11:52 +0300

    Fade away navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
 import {Link} from 'react-router-dom';
 
 
 function Nav() {
+
+    const [navBar, setNavBar] = useState(false);
+
+    const changeBackground = () => {
+        if(window.scrollY > 50) {
+            setNavBar(true)
+        } else {
+            setNavBar(false)
+        }
+    };
+
+    window.addEventListener('scroll', changeBackground)
+
     return (
-        <nav>
+        <nav className={navBar ? 'nav-bar active' : 'nav-bar'}>
             <Link to="/">
                 <h3>Logo</h3>
             </Link>
