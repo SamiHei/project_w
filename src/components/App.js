ca159afe4ead0d47e2d37623253dad6f7b7e3181
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-05-17 17:59:59 +0300

    Publish to github

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <div className="App">
                 <Nav />
                 <Switch>
