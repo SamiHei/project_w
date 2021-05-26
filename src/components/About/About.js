e4a4ff1cd8c10a7eaaadd159d8ff0a9956e5064f
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-05-26 18:56:58 +0300

    Some style fixing

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,13 +7,12 @@ import data from '../../content/about_me.json';
 function About() {
 return (
     <div className="page-2">
+      <div className="about-me-header">{data.header}</div>
       <div className="container-left">
-        <div className="header-text">{data.header}</div>
         <div className="container-left-text-part-one">{data.aboutMeFirstPart}</div>
         <div className="container-left-text-part-two">{data.aboutMeSecondPart}</div>
       </div>
       <div className="container-right">
-      <div className="header-text">{data.headerTwo}</div>
         <div className="container-right-text-part-one">{data.lifeGoalsFirstPart}</div>
         <div className="container-right-text-part-two">{data.lifeGoalsSecondPart}</div>
       </div>
