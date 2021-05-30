9896aa01c443f0f1d51679e97ea12df9354b2809
Sami Heikkinen <zami.heikkinen@hotmail.com>
2021-05-30 18:50:44 +0300

    Fixed about me texts

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,10 +9,10 @@ return (
     <div id="page-2">
       <div className="container-upper">
         <div className="about-me-header">{data.header}</div>
-        <div className="text-container">{data.aboutMe1}{data.aboutMe2}</div>
+        <div className="text-container">{data.aboutMe1} {data.aboutMe2}</div>
       </div>
       <div className="container-lower">
-        <div className="text-container">{data.aboutMe3}{data.aboutMe4}</div>
+        <div className="text-container">{data.aboutMe3} {data.aboutMe4}</div>
       </div>
     </div>
   );
